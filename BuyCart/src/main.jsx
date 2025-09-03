9971e55070cddf56fac65d01aab7c521009532be
sri_vishnu_03 <168799895+srivishnu123@users.noreply.github.com>
2025-09-03 09:25:58 +0530

    Update main.jsx

--- BuyCart/src/main.jsx
+++ BuyCart/src/main.jsx
@@ -9,7 +9,6 @@ import { ToastContainer } from 'react-toastify'
 import ScrollToTop from 'react-scroll-to-top'
 
 // Import your Publishable Key
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key")
@@ -19,7 +18,7 @@ createRoot(document.getElementById('root')).render(
   // <StrictMode>
   <DataProvider>
     <CartProvider>
-      <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+      <ClerkProvider publishableKey={pk_test_d2FybS1zd2lmdC0xOS5jbGVyay5hY2NvdW50cy5kZXYk} afterSignOutUrl="/">
         <App />
         <ScrollToTop color='white' smooth style={{backgroundColor:'#fa2d37', display:'flex', alignItems:'center', justifyContent:'center'}}/>
         <ToastContainer
